566fcd33a5947392d0ace03b44d753d6abec313b
TKD_Kedis <79972336+KedisPL@users.noreply.github.com>
2025-11-13 21:52:58 +0100

    Update constants.js

--- dist/constants.js
+++ dist/constants.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SOURCE_URL = exports.TMDB_META_URL = void 0;
 exports.TMDB_META_URL = "https://94c8cb9f702d-tmdb-addon.baby-beamup.club/meta";
-exports.SOURCE_URL = "https://vidsrc-embed.ru/embed";
+exports.SOURCE_URL = "https://vidsrc.xyz/embed";
